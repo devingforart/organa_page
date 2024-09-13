6dc369567de3b71c53683958f14f3b2621ebe55b
unknown <devingforart@gmail.com>
2024-09-13 17:30:08 -0300

    organa img landing

--- src/components/ImageLanding.tsx
+++ src/components/ImageLanding.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import './ImageLanding.css';
 
-const imageSrc = 'src/assets/images/organa_landing.png'
+const imageSrc = 'src/assets/images/organa_landing.png';
 
 const Intro: React.FC = () => {
   return (
     <div className="intro-container">
-      <img src={imageSrc} alt={'imageLanding'} style={{ width:'1200px', height:'800px', objectFit:'contain' }} />
-
+      <img className="landing-image" src={imageSrc} alt="Organa Landing" />
     </div>
   );
 };
